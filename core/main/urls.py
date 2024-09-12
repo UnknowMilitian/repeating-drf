54033fa47b4d141e012e099b4d68dc9ec0eef3d0
UnknowMilitiant <j.umariy4238@gmail.com>
2024-09-12 17:59:20 +0500

    Some changes

--- core/main/urls.py
+++ core/main/urls.py
@@ -1,18 +1,26 @@
 from django.urls import path
 from .views import (
-    CategoryListCreateAPIView,
-    BlogListCreateAPIView,
+    CategoryListAPIView,
+    CategoryCreateAPIView,
+    CategoryRetrieveAPIView,
     CategoryRetrieveUpdateDestroyAPIView,
+    BlogListCreateAPIView,
     BlogRetrieveUpdateDestroyAPIView,
 )
 
 urlpatterns = [
-    path("categories", CategoryListCreateAPIView.as_view(), name="category-list"),
-    path("blogs", BlogListCreateAPIView.as_view(), name="blog-list"),
+    path("category-list", CategoryListAPIView.as_view(), name="category-list"),
+    path("category-create", CategoryCreateAPIView.as_view(), name="category-create"),
     path(
-        "category/<int:pk>",
-        CategoryRetrieveUpdateDestroyAPIView.as_view(),
-        name="category-list",
+        "category-detail/<int:pk>",
+        CategoryRetrieveAPIView.as_view(),
+        name="category-detail",
     ),
+    path(
+        "category-update/<int:pk>",
+        CategoryRetrieveUpdateDestroyAPIView.as_view(),
+        name="category-update",
+    ),
+    path("blogs", BlogListCreateAPIView.as_view(), name="blog-list"),
     path("blog/<int:pk>", BlogRetrieveUpdateDestroyAPIView.as_view(), name="blog-list"),
 ]
--- core/main/views.py
+++ core/main/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics
+from rest_framework import permissions
+
 
 from .models import Category, Blog
 from .serializers import CategorySerializer, BlogSerializer
 
 
-class CategoryListCreateAPIView(ListCreateAPIView):
+class CategoryListAPIView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class BlogListCreateAPIView(ListCreateAPIView):
+class CategoryCreateAPIView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CategoryRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class BlogListCreateAPIView(generics.ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
 
-class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-
-class BlogRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+class BlogRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
