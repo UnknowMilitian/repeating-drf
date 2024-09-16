14889f52992729ef4def47e94200772ccbdb3874
UnknowMilitiant <j.umariy4238@gmail.com>
2024-09-16 17:08:47 +0500

    Some changes

--- core/main/admin.py
+++ core/main/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
-from .models import Category, Blog
+from django.utils.translation import gettext_lazy as _
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Token, Category, Blog
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("password",),
+            },
+        ),
+        (_("Personal info"), {"fields"}, ("phone", "full_name")),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        None,
+        {"classes": ("wide",), "fields": ("usable_password", "password1", "password2")},
+    )
+
+    list_display = ("email", "full_name", "is_staff")
+    search_fields = ("full_name", "email")
+    ordering = ("full_name",)
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Category)
--- core/main/serializers.py
+++ core/main/serializers.py
@@ -1,5 +1,36 @@
 from rest_framework import serializers
-from .models import Category, Blog
+from rest_framework import exceptions
+from .models import User, Token, Category, Blog
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "phone", "full_name", "is_staff", "is_superuser")
+
+
+class LoginConfirmSerializer(serializers.Serializer):
+    code = serializers.CharField(max_length=6)
+    phone = serializers.CharField(max_length=20)
+
+
+class PhoneAndPasswordSerializer(serializers.Serializer):
+    phone = serializers.CharField(max_length=20)
+    password = serializers.CharField(max_length=255)
+
+    def validate_password(self, value):
+        if len(value) > 8:
+            raise exceptions.ValidationError(
+                "Password must be at least 8 characters long"
+            )
+        return value
+
+    def validate(self, attrs):
+        print(attrs)
+
+        if not attrs.get("phone").lstrip("+").isdigit():
+            raise exceptions.ValidationError("Invalid phone number")
+        return attrs
 
 
 class CategorySerializer(serializers.ModelSerializer):
